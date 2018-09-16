3be28b74975b7693b7cd4035a871bcc5816efdc2
Mr.wang <jiucheng0605@gmail.com>
2018-09-16 09:51:09 +0800

    add opn for auto open url

--- koa2-demo/server.js
+++ koa2-demo/server.js
@@ -2,33 +2,38 @@ const Koa = require("koa")
 const app = new Koa()
 
 let port = 3000
+var opn = require('opn')
+var uri = 'http://localhost:' + port
 
 
-// app.use(async (ctx) => {
-//     ctx.body = "Hello Koa2"
-// })
+
+app.use(async (ctx) => {
+    ctx.body = "Hello Koa2"
+})
 
 
 // 输出：135642
-app.use(async (ctx, next) => {
-    ctx.body = "1"
-    next()
-    ctx.body += "2"
-})
+// app.use(async (ctx, next) => {
+//     ctx.body = "1"
+//      //下一个中间件
+//     next()
+//     ctx.body += "2"
+// })
 
-app.use(async (ctx, next) => {
-    ctx.body += "3"
-    next()
-    ctx.body += "4"
-})
+// app.use(async (ctx, next) => {
+//     ctx.body += "3"
+//     next()
+//     ctx.body += "4"
+// })
 
-app.use(async (ctx, next) => {
-    ctx.body += "5"
-    next()
-    ctx.body += "6"
-})
+// app.use(async (ctx, next) => {
+//     ctx.body += "5"
+//     next()
+//     ctx.body += "6"
+// })
 
 
 app.listen(port, () => {
     console.log("listening on .."+port)
+    opn(uri)
 })
